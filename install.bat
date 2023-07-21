319291b3e33251593aa6ff3f7fae9a900eadfe24
Cameron <me@cameronhalter.com>
2023-07-21 13:44:57 -0400

    Change App Location

--- install.bat
+++ install.bat
@@ -2,10 +2,10 @@
 echo Building Uni-Sync
 cargo build --release
 if exist target\release\uni-sync.exe (
-    mkdir "%APPDATA%\Local\uni-sync" 2> NUL
-    move "target\release\uni-sync.exe" "%APPDATA%\Local\uni-sync"
-    xcopy /d "uni-sync.json" "%APPDATA%\Local\uni-sync"
-    reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v UniSync /d "%APPDATA%\Local\uni-sync" /f
+    mkdir "%APPDATA%\..\Local\uni-sync" 2> NUL
+    move "target\release\uni-sync.exe" "%APPDATA%\..\Local\uni-sync"
+    xcopy /d "uni-sync.json" "%APPDATA%\..\Local\uni-sync"
+    reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v UniSync /d "%APPDATA%\..\Local\uni-sync" /f
     @RD /S /Q "target"
     echo Build Complete! Added uni-sync.exe to Startup
 ) else (
--- uninstall.bat
+++ uninstall.bat
@@ -1,9 +1,9 @@
 @echo off
 echo Removing Uni-Sync
 cargo build --release
-if exist "%APPDATA%\Local\uni-sync" (
-    echo f | xcopy /f /Y "%APPDATA%\Local\uni-sync\uni-sync.json" ".\uni-sync-backup.json"
-    @RD /S /Q "%APPDATA%\Local\uni-sync"
+if exist "%APPDATA%\..\Local\uni-sync" (
+    echo f | xcopy /f /Y "%APPDATA%\..\Local\uni-sync\uni-sync.json" ".\uni-sync-backup.json"
+    @RD /S /Q "%APPDATA%\..\Local\uni-sync"
 )
 reg delete HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v UniSync /f
 echo Removed Uni-Sync
