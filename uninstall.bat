d35a78323b608c8cadc1216445a085a6ab1b749b
Cameron <me@cameronhalter.com>
2023-07-21 13:22:29 -0400

    Improve Windows Uninstall

--- uninstall.bat
+++ uninstall.bat
@@ -2,7 +2,7 @@
 echo Removing Uni-Sync
 cargo build --release
 if exist "%APPDATA%\Local\uni-sync" (
-    xcopy /Y "%APPDATA%\Local\uni-sync\uni-sync.json" "uni-sync-backup.json"
+    echo f | xcopy /f /Y "%APPDATA%\Local\uni-sync\uni-sync.json" ".\uni-sync-backup.json"
     @RD /S /Q "%APPDATA%\Local\uni-sync"
 )
 reg delete HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v UniSync /f
