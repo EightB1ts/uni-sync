b0350c9b7a2211057759e66e2870a61f5aee7cf4
Brandon Carvalho <bcarvalho2500@gmail.com>
2024-01-19 23:27:18 -0500

    Change storage location and fix service not starting

--- install.sh
+++ install.sh
@@ -2,60 +2,62 @@
 
 # Check for Rust Function
 check_rust() {
-    echo '🦀 Checking for Rust'
-    if ! command -v rustc &> /dev/null; then
-        echo '⛔ Could not locate Rust Compiler ⛔'
-        echo 'Try running:'
-        echo 'curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh'
-        exit
-    else
-        echo '✅ Its Rusty!'
-    fi
+	echo '🦀 Checking for Rust'
+	if ! command -v rustc &>/dev/null; then
+		echo '⛔ Could not locate Rust Compiler ⛔'
+		echo 'Try running:'
+		echo 'curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh'
+		exit
+	else
+		echo '✅ Its Rusty!'
+	fi
 }
 
 build_app() {
 
-    if [ -f "uni-sync" ]; then
-        rm uni-sync
-    fi
+	if [ -f "uni-sync" ]; then
+		rm uni-sync
+	fi
 
-    echo '⚒️  Building Uni-Sync'
-    cargo build --release
+	echo '⚒️  Building Uni-Sync'
+	cargo build --release
 
-    if [ ! -f "target/release/uni-sync" ]; then
-        echo '⛔ Build Failed ⛔'
-        exit
-    fi
+	if [ ! -f "target/release/uni-sync" ]; then
+		echo '⛔ Build Failed ⛔'
+		exit
+	fi
 
-    mv target/release/uni-sync .
-    rm -rf ./target
+	mv target/release/uni-sync .
+	rm -rf ./target
 
 }
 
 install_app() {
-    cat > 'uni-sync.service' << SERVICE
+	cat >'uni-sync.service' <<SERVICE
 [Unit]
 Description=Uni-Sync service
 [Service]
-ExecStart=/usr/sbin/uni-sync
+ExecStart=/usr/local/bin/uni-sync
 [Install]
 WantedBy=multi-user.target
 SERVICE
-    sudo mv -f uni-sync.service /etc/systemd/system
-    sudo mv -f uni-sync /usr/sbin
-    sudo cp -n uni-sync.json /usr/sbin
-    sudo chown $USER /usr/sbin/uni-sync.json
-    sudo systemctl enable uni-sync
-    sudo systemctl restart uni-sync
+	sudo mv -f uni-sync.service /usr/lib/systemd/system
+	sudo mv -f uni-sync /usr/local/bin
+	sudo cp -n uni-sync.json /usr/local/bin
+	sudo chown $USER /usr/local/bin/uni-sync.json
+	sudo restorecon /usr/lib/systemd/system/uni-sync.service
+	sudo restorecon /usr/local/bin/uni-sync
+	sudo restorecon /usr/local/bin/uni-sync.json
+	sudo systemctl daemon-reload
+	sudo systemctl enable uni-sync
+	sudo systemctl restart uni-sync
 }
-
 
 check_rust
 build_app
 
 read -p "Would you like to install as Service? [N/y]: " -n 1 -r
-echo 
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    install_app
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	install_app
 fi
